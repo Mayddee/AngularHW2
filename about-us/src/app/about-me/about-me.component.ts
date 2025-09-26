bbbf3edfa5d6befa802d8ce49ef5ea0f40375556
Mayddee <a.madikosh2004@gmail.com>
2025-09-26 09:53:00 +0500

    commit

---
+++ about-us/src/app/about-me/about-me.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-about-me',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './about-me.component.html',
+  styleUrl: './about-me.component.css'
+})
+export class AboutMeComponent { 
+  studentInfo = {
+    name: 'Амангелді Мадина',
+    title: 'Студент 4 курса',
+    university: 'КБТУ',
+    major: 'Информационные Системы',
+    bio: 'Привет! Я изучаю информационные системы в КБТУ.',
+    
+    contacts: [
+      {
+        icon: 'fa-phone',
+        label: 'Телефон',
+        value: '+7 (707) 906-20-51',  
+        link: ''     
+      },
+      {
+        icon: 'fa-envelope',
+        label: 'Email',
+        value: 'm_amangeldi@kbtu.kz',  
+        link: ''
+      }
+      
+    ]
+  };
+
+  constructor() { }
+
+  onContactClick(contact: any) {
+    if (contact.link) {
+      window.open(contact.link, '_blank');
+    }
+  }
+}
